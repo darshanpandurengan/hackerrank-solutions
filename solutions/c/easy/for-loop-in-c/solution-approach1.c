9cfa24ba3c178e3f34f1238c0865c873ed563d82
Darshan P <225718512+darshanpandurengan@users.noreply.github.com>
2026-07-18 16:52:04 +0530

    ✅ For Loop in C [c] — approach1

---
+++ solutions/c/easy/for-loop-in-c/solution-approach1.c
@@ -0,0 +1,67 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/for-loop-in-c/problem?isFullScreen=true
+// Problem     For Loop in C
+// Difficulty  Easy
+// Subdomain   Conditionals and Loops
+// Platform    HackerRank
+// Language    c
+// Status      Accepted
+// Submitted   2026-07-18, 04:52 p.m.
+// ──────────────────────────────────────────────────
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+      // Complete the code.
+    for(int i = a ; i <= b ; i++){
+        if (i < 10){
+        switch(i) 
+        {
+            case 1 :
+            printf("one\n") ;
+            break ; 
+            case 2 :
+            printf("two\n") ;
+            break ;
+            case 3 :
+            printf("three\n") ;
+            break ;
+            case 4 :
+            printf("four\n") ;
+            break ;
+            case 5 :
+            printf("five\n") ;
+            break ;
+            case 6 :
+            printf("six\n") ;
+            break ;
+            case 7 :
+            printf("seven\n") ;
+            break ;
+            case 8 :
+            printf("eight\n") ;
+            break ;
+            case 9 :
+            printf("nine\n") ;
+            break ;
+        }
+        }
+        else {
+            if (i % 2 == 0 ){
+                printf("even\n") ; 
+            }
+            else {
+                printf("odd\n") ; 
+            }
+        }
+    }
+    return 0;
+}
