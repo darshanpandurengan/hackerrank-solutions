6b7c8f271c5ebc6c5b1c62110e4d91efefe460eb
Darshan P <225718512+darshanpandurengan@users.noreply.github.com>
2026-07-18 16:21:13 +0530

    ✅ Playing With Characters [c] — approach1

---
+++ solutions/c/easy/playing-with-characters/solution-approach1.c
@@ -0,0 +1,31 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/playing-with-characters/problem?isFullScreen=true
+// Problem     Playing With Characters
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    c
+// Status      Accepted
+// Submitted   2026-07-18, 04:21 p.m.
+// ──────────────────────────────────────────────────
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c ; 
+    scanf("%c", &c);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char s[100] ;
+    scanf("%s" , &s) ;
+    getchar();  
+    char sen[100] ;
+    fgets(sen, sizeof(sen), stdin);
+    printf("%c\n" , c) ;
+    printf("%s\n" , s) ;
+    printf("%s" , sen ) ;
+    return 0;
+}
